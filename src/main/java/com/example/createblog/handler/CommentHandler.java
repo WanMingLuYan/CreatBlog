52f9e1e7a4314956cc48e8765ad9efd88e1512d5
WanMingLuYan <w1824786966@163.com>
2024-11-26 20:39:51 +0800

    2024-11-26

---
+++ src/main/java/com/example/createblog/handler/CommentHandler.java
@@ -0,0 +1,50 @@
+package com.example.createblog.handler;
+
+import com.example.createblog.model.Article;
+import com.example.createblog.model.Comment;
+import com.example.createblog.model.User;
+import com.example.createblog.repository.ArticleRepository;
+import com.example.createblog.repository.CommentRepository;
+import com.example.createblog.repository.UserRepository;
+import com.example.createblog.security.Secured;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/comments")
+public class CommentHandler {
+
+    @Inject
+    private CommentRepository commentRepository;
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private ArticleRepository articleRepository;
+
+    @POST
+    @Path("/")
+    @Secured({"user", "admin"})
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createComment(Comment comment , @Context SecurityContext securityContext ) {
+        User user = userRepository.findByID(Integer.valueOf(securityContext.getUserPrincipal().getName()));
+        comment.setUser(user);
+        Article article = articleRepository.findByID(comment.getArticleId());
+        comment.setArticle(article);
+        comment.setCreatedAt(System.currentTimeMillis() / 1000);
+        commentRepository.create(comment);
+        Map<String, Object> res = new HashMap<>();
+        res.put("code", Response.Status.OK);
+        return Response.status(Response.Status.OK).entity(res).build();
+    }
+
+}
+
---
+++ src/main/java/com/example/createblog/util/BCryptUtil.java
@@ -0,0 +1,16 @@
+package com.example.createblog.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptUtil {
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+}
+
